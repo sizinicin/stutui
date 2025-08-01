80402d45c290df4cfe499e437be8cc28b90ae290
Arda Yılmaz <yilmaz.ar@proton.me>
2025-08-01 18:43:46 +0300

    Add GPL-3.0 header to stutui.c

--- stutui.c
+++ stutui.c
@@ -1,3 +1,20 @@
+/*
+ * stutui - A minimalist terminal‐based study timer and stopwatch with daily logging, built on ncurses.
+ * Copyright (C) 2025  Arda Yılmaz
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <ncurses.h>
 
 #include <unistd.h>
